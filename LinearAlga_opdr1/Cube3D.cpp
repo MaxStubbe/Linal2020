6134477523c5cc641f080d36e28de8590303d0f9
ROBIN-PC\robin <rm.hobbel@telfort.nl>
2020-01-10 15:26:31 +0100

    Finished Cube3D data

--- LinearAlga_opdr1/Cube3D.cpp
+++ LinearAlga_opdr1/Cube3D.cpp
@@ -2,23 +2,53 @@
 
 Cube3D::Cube3D(Camera3D& camera, int size) : Object3D(camera)
 {
-	//Side 1: front
-	std::vector<Vector3D> side_1 = {
+	std::vector<Vector3D> front = {
 		Vector3D(0, 0, 0),
-		Vector3D(0, size, 0),
+		Vector3D(size, 0, 0),
 		Vector3D(size, size , 0),
-		Vector3D(size, 0, 0)
+		Vector3D(0, size, 0)
 	};
-	points_.push_back(side_1);
+	points_.push_back(front);
 
-	//Side 2: top
-	std::vector<Vector3D> side_2 = {
+	std::vector<Vector3D> back = {
+		Vector3D(0, 0, size),
+		Vector3D(size, 0, size),
+		Vector3D(size, size , size),
+		Vector3D(0, size, size)
+	};
+	points_.push_back(back);
+
+	std::vector<Vector3D> left = {
+		Vector3D(0, 0, size),
 		Vector3D(0, 0, 0),
 		Vector3D(0, size, 0),
-		Vector3D(0, size , size),
-		Vector3D(0, 0, size)
+		Vector3D(0, size, size)
 	};
-	points_.push_back(side_2);
+	points_.push_back(left);
+
+	std::vector<Vector3D> right = {
+		Vector3D(size, 0, size),
+		Vector3D(size, 0, 0),
+		Vector3D(size, size, 0),
+		Vector3D(size, size, size)
+	};
+	points_.push_back(right);
+
+	std::vector<Vector3D> top = {
+		Vector3D(0, 0, size),
+		Vector3D(size, 0, size),
+		Vector3D(size, 0, 0),
+		Vector3D(0, 0, 0)
+	};
+	points_.push_back(top);
+
+	std::vector<Vector3D> bottom = {
+		Vector3D(0, size, size),
+		Vector3D(size, size, size),
+		Vector3D(size, size, 0),
+		Vector3D(0, size, 0)
+	};
+	points_.push_back(bottom);
 }
 
 void Cube3D::update()
